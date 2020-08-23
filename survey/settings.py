bf7dd6aa13f035892d63aa97102326460901f08a
nkpatil <patilnaresh22@gmail.com>
2020-08-24 02:45:32 +0530

    updated db setings and requirements

--- survey/settings.py
+++ survey/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'survey.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=500)
+    {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 }
 
 
